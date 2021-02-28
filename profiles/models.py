b22404dcc919d4e1ff74fb96d54f61b2dfb9eae5
Michael Jarvis <mjarvis9541@gmail.com>
2021-02-28 12:57:16 +0000

    Formatted code

--- profiles/models.py
+++ profiles/models.py
@@ -11,6 +11,7 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
 from progress.models import Progress
 from utils.behaviours import Nutritionable, Uuidable
 
@@ -54,16 +55,12 @@ class Profile(Uuidable):
         null=True,
         blank=True,
     )
-
+    
     # Base user stats
     sex = models.CharField(max_length=1, choices=Sex.choices, null=True, blank=True)
     height = models.IntegerField('height (cm)', null=True, blank=True)
-    weight = models.DecimalField(
-        'weight (kg)', max_digits=4, decimal_places=1, null=True, blank=True
-    )
-    date_of_birth = models.DateField(
-        null=True, blank=True, validators=[MaxValueValidator(limit_value=date.today)]
-    )
+    weight = models.DecimalField('weight (kg)', max_digits=4, decimal_places=1, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True, validators=[MaxValueValidator(limit_value=date.today)])
     activity_level = models.CharField(
         max_length=2,
         choices=ActivityLevel.choices,
@@ -113,27 +110,13 @@ class Profile(Uuidable):
         help_text="Records the method that was used to set the user's macronutrient targets.",
     )
     energy = models.IntegerField(verbose_name='energy (kcal)', default=2000)
-    fat = models.DecimalField(
-        verbose_name='fat (g)', max_digits=4, decimal_places=1, default=70
-    )
-    saturates = models.DecimalField(
-        verbose_name='saturates (g)', max_digits=4, decimal_places=1, default=20
-    )
-    carbohydrate = models.DecimalField(
-        verbose_name='carbohydrate (g)', max_digits=4, decimal_places=1, default=260
-    )
-    sugars = models.DecimalField(
-        verbose_name='sugars (g)', max_digits=4, decimal_places=1, default=90
-    )
-    fibre = models.DecimalField(
-        verbose_name='fibre (g)', max_digits=4, decimal_places=1, default=30
-    )
-    protein = models.DecimalField(
-        verbose_name='protein (g)', max_digits=4, decimal_places=1, default=50
-    )
-    salt = models.DecimalField(
-        verbose_name='salt (g)', max_digits=5, decimal_places=2, default=6
-    )
+    fat = models.DecimalField(verbose_name='fat (g)', max_digits=4, decimal_places=1, default=70)
+    saturates = models.DecimalField(verbose_name='saturates (g)', max_digits=4, decimal_places=1, default=20)
+    carbohydrate = models.DecimalField(verbose_name='carbohydrate (g)', max_digits=4, decimal_places=1, default=260)
+    sugars = models.DecimalField(verbose_name='sugars (g)', max_digits=4, decimal_places=1, default=90)
+    fibre = models.DecimalField(verbose_name='fibre (g)', max_digits=4, decimal_places=1, default=30)
+    protein = models.DecimalField(verbose_name='protein (g)', max_digits=4, decimal_places=1, default=50)
+    salt = models.DecimalField(verbose_name='salt (g)', max_digits=5, decimal_places=2, default=6)
 
     __original_weight = None
 
@@ -151,9 +134,7 @@ class Profile(Uuidable):
                 post.save()
                 print('updated user weight within progress model on this date')
             else:
-                Progress.objects.create(
-                    user=self.user, date=timezone.now(), weight=self.weight
-                )
+                Progress.objects.create(user=self.user, date=timezone.now(), weight=self.weight)
                 print('created user weight within progress within on this date')
         super().save(*args, **kwargs)
 
@@ -169,23 +150,21 @@ class Profile(Uuidable):
         if self.salt is not None:
             return round(self.salt * 400)
 
-    # Basic calculations / properties
-
     @property
     def weight_lb(self):
-        # Converts users weight from kg to lb
+        # Converts user's weight from kg to lb
         if self.weight:
             return round(self.weight * 2.20462)
 
     @property
     def goal_weight_lb(self):
-        # Converts users weight from kg to lb
+        # Converts user's weight from kg to lb
         if self.goal_weight:
             return round(self.goal_weight * 2.20462)
 
     @property
     def weight_st(self):
-        # Converts users weight from kg to st and lb
+        # Converts user's weight from kg to st and lb
         if self.weight_lb:
             weight = {}
             weight['st'] = round(self.weight_lb // 14)
@@ -194,7 +173,7 @@ class Profile(Uuidable):
 
     @property
     def goal_weight_st(self):
-        # Converts users goal weight from kg to st and lb
+        # Converts user's goal weight from kg to st and lb
         if self.goal_weight_lb:
             weight = {}
             weight['st'] = round(self.goal_weight_lb // 14)
@@ -203,13 +182,13 @@ class Profile(Uuidable):
 
     @property
     def height_in(self):
-        # Converts users height from cm to in
+        # Converts user's height from cm to in
         if self.height:
             return round(self.height / 2.54)
 
     @property
     def height_ft(self):
-        # Converts users height from cm to ft and in
+        # Converts user's height from cm to ft and in
         if self.height_in:
             height = {}
             height['ft'] = round(self.height_in // 12)
@@ -218,55 +197,45 @@ class Profile(Uuidable):
 
     @property
     def age(self):
-        # Calculates users current age based on their date of birth 
+        # Calculates user's current age from their date of birth
         if self.date_of_birth:
             now = timezone.now()
             return (
                 now.year
                 - self.date_of_birth.year
-                - (
-                    (now.month, now.day)
-                    < (self.date_of_birth.month, self.date_of_birth.day)
-                )
+                - ((now.month, now.day) < (self.date_of_birth.month, self.date_of_birth.day))
             )
 
     @property
     def goal_duration_weeks(self):
-        # Calculates how long it will reasonably take the user to go
-        # from their current weight to their goal weight (in weeks).
+        # Calculates how long it will take the user to reach their goal weight in weeks
         if self.weight:
             duration = {}
             duration['short'] = abs(self.weight - self.goal_weight) / 1
             duration['long'] = abs(self.weight - self.goal_weight) / 0.5
             return duration
 
-    # Calculations for BMI, BMR and TDEE
     @property
     def bmi(self):
-        # Body Mass Index
+        # Calculates Body Mass Index
         if self.weight and self.height:
             return round(self.weight / Decimal(((self.height / 100) ** 2)), 1)
 
     @property
     def bmr(self):
-        # Basal Metablic Rate
-        # Calculated using the Harris–Benedict Equation, revised by Mifflin and St Jeor
+        # Calculates Basal Metablic Rate
+        # Uses the Harris–Benedict Equation, revised by Mifflin and St Jeor
         if self.weight and self.height:
             modifier = 0
             if self.sex == self.Sex.MALE:
                 modifier = 5
             elif self.sex == self.Sex.FEMALE:
                 modifier = -161
-            return round(
-                (10 * self.weight)
-                + Decimal(6.25 * self.height)
-                - (5 * self.age)
-                + modifier
-            )
+            return round((10 * self.weight) + Decimal(6.25 * self.height) - (5 * self.age) + modifier)
 
     @property
     def tdee(self):
-        # Total Daily Energy Expenditure
+        # Calculates Total Daily Energy Expenditure
         if self.activity_level and self.bmr:
             modifier = 0
             if self.activity_level == self.ActivityLevel.SEDENTARY:
@@ -280,10 +249,10 @@ class Profile(Uuidable):
             elif self.activity_level == self.ActivityLevel.EXTRA_ACTIVE:
                 modifier = 1.9
             return round(self.bmr * modifier)
-    
+
     @property
     def recommended_calories(self):
-        # Sets the users recommended calorie target based on their 
+        # Sets the users recommended calorie target based on their
         # tdee and goals.
         if self.goal and self.tdee:
             modifier = 0
